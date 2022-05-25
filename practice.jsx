2ce5fd282aaec9b7eba3105cd86a02a6cc429817
RHBusch <91342001+RHBusch@users.noreply.github.com>
2022-05-24 21:01:04 -0400

    Practicing for in loop

--- practice.jsx
+++ practice.jsx
@@ -75,3 +75,46 @@ const title = ""
 const animalFacts = <h1>{(title === '' ? 'Click an animal for a fun fact' : title)}</h1>
 
 ReactDOM.render(animalFacts, document.getElementById("root"))
+
+//Practicing for in loop 
+
+const images = [];
+
+for (const animal in animals) {
+    images.push(
+        <img
+            key={animal}
+            className='animal'
+            alt={animal}
+            src={animals[animal].image}
+            ariaLabel={animal}
+            role='button'
+        />
+    )
+}
+
+const animalFacts =
+    <div>
+
+        <h1>{(title === '' ? 'Click an animal for a fun fact' : title)}</h1>
+        {background}
+        <div className="animals">{images}</div>
+    </div>
+
+ReactDOM.render(animalFacts, document.getElementById("root"))
+
+export const animals = {
+    dolphin: {
+        image: '/images/dolphin.jpg',
+        facts: ['Dolphins have been shown to give distinct names to each other!', 'Dolphins are known to display their own culture!', 'Dolphins have two stomachs!']
+    },
+    lobster: {
+        image: '/images/lobster.jpg',
+        facts: ['Lobsters taste with their legs!', 'Lobsters chew with their stomachs!', 'Lobsters can live as long as 100 years.']
+    },
+    starfish: {
+        image: '/images/starfish.jpg',
+        facts: ['Starfish can have up to 40 arms!', 'Starfish have no brain and no blood!', 'Starfish can regenerate their own arms!']
+    }
+};
+//
